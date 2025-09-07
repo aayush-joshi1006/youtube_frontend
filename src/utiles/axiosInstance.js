ff73f08adbd7081cb491e6de0bed555a573c63c9
Aayush Joshi <aayushjoshi1006@gmail.com>
2025-09-07 11:12:13 +0530

    added production url

--- src/utiles/axiosInstance.js
+++ src/utiles/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Axios instance for making request to the backend
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://youtube-backend-cr9n.onrender.com/api",
   withCredentials: true,
 });
 
